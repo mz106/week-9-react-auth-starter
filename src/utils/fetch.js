b41d048974c7136cc30c671cdaa807fe620958ae
Samm Wiltshire <samm94tw@gmail.com>
2024-09-26 13:47:35 +0100

    sign up etch working

---
+++ src/utils/fetch.js
@@ -0,0 +1,26 @@
+export const signupFetch = async (username, email, password) => {
+    try {
+        const response = await fetch("http://localhost:5001/users/signup", {
+            method: "POST", 
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username, 
+                email: email, 
+                password: password,
+                        }),
+        });
+
+        const data = await response.json()
+        console.log("data in signupFetch:", data);
+
+        return data; 
+
+    } catch (error) {
+        console.log(error);
+        alert("Error. PLEASE cheack console.");
+    }
+};
+
